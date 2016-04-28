a2e8f193933f205b7fdb968c3fe3d26c0992213d
reorx <novoreorx@gmail.com>
2016-04-28 11:37:43 +0800

    update hs userdict for old gods cards

--- hearthstone_userdict.py
+++ hearthstone_userdict.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import re
+import sys
 import json
 import itertools
 
@@ -12,38 +13,48 @@ OUTPUT_FILEPATH = 'Rime/custom_dict/luna_pinyin.hs.dict.yaml'
 
 
 if __name__ == '__main__':
-    with open('AllSets.zhCN.json', 'r') as f:
+    CMDS = ['test', 'write']
+    cmd = sys.argv[1]
+    if cmd not in CMDS:
+        print 'Invalid command, available commands: {}'.format(CMDS)
+        sys.exit(1)
+
+    with open('cards.json', 'r') as f:
         text = f.read()
 
-    d = json.loads(text)
+    cards = json.loads(text)
     cardset = set()
 
     number_regex = re.compile(r'[0-9]')
     abc_regex = re.compile(r'[a-zA-Z]')
 
-    for sname, cards in d.iteritems():
-        #print sname
-        for card in cards:
-            _name = card['name']
+    for card in cards:
+        _name = card['name']
 
-            # Remove puncturator
-            name = parser.remove_punc(_name)
+        # Remove puncturator
+        name = parser.remove_punc(_name)
 
-            # Remove number
-            name = re.sub(number_regex, '', name)
-            if not name:
-                continue
+        # Remove number
+        name = re.sub(number_regex, '', name)
+        if not name:
+            continue
 
-            # Remove first-character-is-English word
-            if abc_regex.match(name[0]):
-                continue
+        # Remove first-character-is-English word
+        if abc_regex.match(name[0]):
+            continue
 
-            cardset.add(name)
+        cardset.add(name)
 
-    with open(OUTPUT_FILEPATH, 'w') as f:
-        f.writelines(
-            itertools.chain(
-                parser.generate_head_lines('luna_pinyin.hs', '1.1'),
-                parser.iter_lines(cardset, priority=1)
+    if cmd == 'test':
+        for i in cardset:
+            print i.encode('utf8')
+    else:
+        print 'write to {}'.format(OUTPUT_FILEPATH)
+
+        with open(OUTPUT_FILEPATH, 'w') as f:
+            f.writelines(
+                itertools.chain(
+                    parser.generate_head_lines('luna_pinyin.hs', '1.1'),
+                    parser.iter_lines(cardset, priority=1)
+                )
             )
-        )
